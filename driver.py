738f8cceb24d0fd741c289bdea0b7781a0a7d44c
realVinayak <realvinayak123@gmail.com>
2023-01-27 15:54:53 -0600

    Add driver.py

---
+++ driver.py
@@ -0,0 +1,16 @@
+from Random import Random
+import matplotlib.pyplot as plt
+if __name__ == "__main__":
+    random = Random(76)
+    N = 1000
+    data = {0: 0, 1:0, 2:0}
+    for n in range(N):
+        category = random.Three_Categorical_Distribution([0.3, 0.5])
+        data[category] += 1
+    keys = list(data.keys())
+    values = list(data.values())
+    plt.title("1000 samples")
+    plt.xlabel("Probability of 0, 1, 2 is 0.3, 0.5 and 0.2 respectively")
+    plt.bar(keys, values, width=0.1)
+    plt.savefig('categorical_data.png')
+    print(data)
